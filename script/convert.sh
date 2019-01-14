66d6938c38239f3002f3a5ef8cd1461bd4838bc0
bluesea0125 <31249899+bluesea0125@users.noreply.github.com>
2019-01-14 11:29:25 +0800

    Update convert.sh

--- script/convert.sh
+++ script/convert.sh
@@ -1,13 +1,5 @@
 ###
 grep -nr 'eth\.'
 grep -nr 'eth_'
-###
-find . -name "*.html"|xargs sed -i "s/eth\./owt\./g"
-find . -name "*.js"|xargs sed -i "s/eth\./owt\./g"
-find . -name "*.map"|xargs sed -i "s/eth\./owt\./g"
-find . -name "*.md"|xargs sed -i "s/eth\./owt\./g"
-###
-find . -name "*.html"|xargs sed -i "s/eth_/owt_/g"
-find . -name "*.js"|xargs sed -i "s/eth_/owt_/g"
-find . -name "*.map"|xargs sed -i "s/eth_/owt_/g"
-find . -name "*.md"|xargs sed -i "s/eth_/owt_/g"
+$ find -iregex '.*\.\(html\|js\|map\|md\)$'|xargs sed -i "s/eth\./owt\./g"
+$ find -iregex '.*\.\(html\|js\|map\|md\)$'|xargs sed -i "s/eth_/owt_/g"
