460c044c7a041ec91de5c85ff14bb23e9a21187e
bluesea0125 <31249899+bluesea0125@users.noreply.github.com>
2019-01-14 13:49:26 +0800

    Update rename.py

--- script/rename.py
+++ script/rename.py
@@ -5,7 +5,7 @@
 import os
 
 keyword = 'web3.eth'
-replace_word = 'web.owt'
+replace_word = 'web3.owt'
 for filename in os.listdir("."):
   if filename.startswith(keyword):
     os.rename(filename, replace_word+filename[len(keyword):])
